0ddd1fb73c8c58b45baa4938ba68cedbe99681ba
joseemrc <00jmrc@gmail.com>
2025-02-05 19:49:31 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
-import node from "@astrojs/node"; // ✅ Importar el adapter de Node.js
+import node from "@astrojs/node"; // Adaptador para Render
 
 export default defineConfig({
-  output: 'server',
+  output: "server",
+  adapter: node({ mode: "standalone" }),
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
